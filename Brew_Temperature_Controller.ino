524c8297f5669e111dbc31757524500b79bf8b8c
Martyn Welch <martyn.welch@collabora.com>
2020-07-25 15:00:42 +0100

    Board tweaks and software update
    The use of D2 to control the heater was a mistake as it does not
    provide PWM, switch to using D3.
    
    The chosen MOSFET was not suitable for driving the heater at full
    current and has thus been switched for a more powerful one.
    
    Move the README so that it's more general for the whole project rather
    than just the board design and use Markdown.
    
    Add license to source code.
    
    Add features to the software:
    
    - Drive output with PWM
    - Utilise screen backlight
    - Save set temperature across boots
    - Display percentage power output when on
    - Test for detached thermistor and turn off output as safety measure
    - Allow scrolling of setpoint with long press of buttons
    - Log to serial at approx 1Hz
    
    Signed-off-by: Martyn Welch <martyn.welch@collabora.com>

--- Brew_Temperature_Controller.ino
+++ Brew_Temperature_Controller.ino
@@ -1,7 +1,25 @@
 /*
  * Brewing Temperature Controller
  * 
- * The following libraries need to be installed for this to work:
+ * Copyright (C) 2020 Martyn Welch <martyn@warpedlogic.co.uk>
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; version 2.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ */
+
+/*
+ * The following libraries are used:
  * - NTC Thermistor (MIT): https://github.com/YuriiSalimov/NTC_Thermistor
  * - U8g2 (2-Clause BSD): https://github.com/olikraus/u8g2
  * - FastPID (LGPL v2.1): https://github.com/mike-matera/FastPID
@@ -12,6 +30,7 @@
 #include <Thermistor.h>
 #include <NTC_Thermistor.h>
 #include <FastPID.h>
+#include <EEPROM.h>
 
 #ifdef U8X8_HAVE_HW_SPI
 #include <SPI.h>
@@ -23,6 +42,8 @@
 #define SWITCH_1 5
 #define SWITCH_2 4
 #define THERM_PIN A0
+#define LIGHT_PIN 6
+#define OUTPUT_PIN 3
 
 #define REFERENCE_RESISTANCE   4700
 #define NOMINAL_RESISTANCE     100000
@@ -31,7 +52,10 @@
 
 #define DEBOUNCE_PERIOD 1000
 
-float Kp=0.1, Ki=0.5, Kd=0.1, Hz=10;
+#define LIGHT_PERIOD 2000
+unsigned long light_time = 0;
+
+float Kp=25, Ki=0.2, Kd=0.01, Hz=37;
 int output_bits = 8;
 bool output_signed = false;
 
@@ -42,13 +66,18 @@ U8G2_PCD8544_84X48_F_4W_SW_SPI u8g2(U8G2_R0, /* clock=*/ 13, /* data=*/ 11, /* c
 unsigned long debounce_switch_1 = 0;
 unsigned long debounce_switch_2 = 0;
 
-int setpoint = 42;
+#define SETMAX 40
+#define SETMIN 0
+int setpoint = 25;
 char setchar[4];
 #define NUM_TEMPS 20
 double temps[NUM_TEMPS];
 int temp_cnt = 0;
 int current = 69;
 char curchar[4];
+uint8_t output = 0;
+char outchar[5];
+unsigned long log_time;
 
 int state1;
 int hold1 = 0;
@@ -60,15 +89,27 @@ bool change2 = true;
 
 Thermistor* thermistor;
 
+#define SAVE_MAGIC 0xbe
+#define SAVE_ADDRESS 0
+struct saved_state {
+  int magic;
+  int temp;
+};
 
+struct saved_state saved;
 
 void setup(void) {
+
   u8g2.begin();
 
   Serial.begin(115200);
 
   pinMode(SWITCH_1, INPUT_PULLUP);
   pinMode(SWITCH_2, INPUT_PULLUP);
+  pinMode(LIGHT_PIN, OUTPUT);
+  digitalWrite(LIGHT_PIN, HIGH);
+  pinMode(OUTPUT_PIN, OUTPUT);
+  digitalWrite(OUTPUT_PIN, LOW);
 
   thermistor = new NTC_Thermistor(
     THERM_PIN,
@@ -77,20 +118,33 @@ void setup(void) {
     NOMINAL_TEMPERATURE,
     B_VALUE
   );
+
+  Serial.println("Setpoint, Output, Temperature");
+
+  EEPROM.get(SAVE_ADDRESS, saved);
+
+  if (saved.magic == SAVE_MAGIC) {
+    setpoint = saved.temp;
+  } else {
+    saved.magic = SAVE_MAGIC;
+  }
+
 }
 
+/*
 uint8_t bitmap[] = {0x3F, 0xFF, 0xA5, 0xA5, 0xA1, 0xE1, 0x21, 0x1E};
-
+*/
 void refresh_display(char *curchar, char *setchar, char *powerchar) {
   // Write current temp and units
   u8g2.clearBuffer();
-  u8g2.setFont(u8g2_font_inb30_mn);
+  u8g2.setFont(u8g2_font_inb30_mf);
   u8g2.drawStr(18, 30, curchar);
+  u8g2.setFont(u8g2_font_profont12_tf);
   u8g2.drawGlyph(70, 10, 176);
   u8g2.drawStr(77, 10, "C");
 
   // Draw set temp box
-  u8g2.drawRFrame(15, 34, 28, 14, 3);
+  u8g2.drawRFrame(15, 34, 20, 14, 3);
   u8g2.drawRBox(0, 36, 18, 9, 3);
   u8g2.setFont(u8g2_font_micro_tr);
   u8g2.setDrawColor(0);
@@ -98,21 +152,21 @@ void refresh_display(char *curchar, char *setchar, char *powerchar) {
   u8g2.setDrawColor(1);
 
   // Draw power box
-  u8g2.drawRFrame(59, 34, 25, 14, 3);
-  u8g2.drawRBox(44, 36, 18, 9, 3);
+  u8g2.drawRFrame(51, 34, 33, 14, 3);
+  u8g2.drawRBox(36, 36, 18, 9, 3);
   u8g2.setFont(u8g2_font_micro_tr);
   u8g2.setDrawColor(0);
-  u8g2.drawStr(47, 43,"PWR");
+  u8g2.drawStr(39, 43,"PWR");
   u8g2.setDrawColor(1);
 
   // Power state and set temp
   u8g2.setFont(u8g2_font_profont12_tf);
-  u8g2.drawStr(64, 45, powerchar);
+  u8g2.drawStr(56, 45, powerchar);
   u8g2.drawStr(20, 45, setchar);
-
+/*
   // Beer Mug Graphic
   u8g2.drawBitmap(0,0,1,8,bitmap);
-  
+*/
   // Transfer buffer to the display
   u8g2.sendBuffer();
 }
@@ -122,23 +176,32 @@ void loop(void) {
   unsigned long now;
   int state;
   double tot = 0;
+  double avg_temp;
   char pwrchar[4];
-  uint8_t output;
   double current;
+  uint8_t pcnt;
 
   now = millis();
-
+  
   state = digitalRead(SWITCH_1);
   if (state == state1) {
     if ((now - debounce_switch_1) < DEBOUNCE_PERIOD) {
       if ((state == 0) & (change1 == true)) {
-        setpoint++;
+        if (setpoint < SETMAX)
+          setpoint++;
         change1 = false;
+
       }
-      debounce_switch_2 = now;
+      debounce_switch_1 = now;
       hold1++;
       if ((state == 0) & (hold1 > 10))
-        setpoint++;
+        if (setpoint < SETMAX)
+          setpoint++;
+
+      if (state == 0) {
+        light_time = now;
+        digitalWrite(LIGHT_PIN, LOW);
+      }
     }
   } else {
     debounce_switch_1 = now;
@@ -151,13 +214,20 @@ void loop(void) {
   if (state == state2) {
     if ((now - debounce_switch_2) < DEBOUNCE_PERIOD) {
       if ((state == 0) & (change2 == true)) {
-        setpoint--;
+        if (setpoint > SETMIN)
+          setpoint--;
         change2 = false;
       }
       debounce_switch_2 = now;
       hold2++;
       if ((state == 0) & (hold2 > 10))
-        setpoint--;
+        if (setpoint > SETMIN)
+          setpoint--;
+
+      if (state == 0) {
+        light_time = now;
+        digitalWrite(LIGHT_PIN, LOW);
+      }
     }
   } else {
     debounce_switch_2 = now;
@@ -166,24 +236,57 @@ void loop(void) {
     hold2 = 0;
   }
 
+  saved.temp = setpoint;
+  EEPROM.put(SAVE_ADDRESS, saved);
+
+  if (now - light_time > LIGHT_PERIOD)
+    digitalWrite(LIGHT_PIN, HIGH);
+
   current = thermistor->readCelsius();
   temps[temp_cnt++] = current;
   temp_cnt = temp_cnt % 20;
-  Serial.println(temp_cnt);
+
   for (int i=0; i < NUM_TEMPS; i++)
     tot += temps[i];
 
   output = myPID.step(setpoint, current);
 
-  sprintf(curchar,"%d", round(tot / NUM_TEMPS));
+  avg_temp = tot / NUM_TEMPS;
+
+  if (avg_temp < 0) {
+    sprintf(curchar, "XX");
+  } else {
+    sprintf(curchar,"%d", round(avg_temp));
+  }
+
   itoa(setpoint, setchar, 10);
 
-  if (output == 0)
-    refresh_display(curchar, setchar, "Off");
-  else
-    refresh_display(curchar, setchar, "On");
+  if (output > 255)
+    output = 255;
 
-  Serial.print("Temperature: ");
-  Serial.print(current);
-  Serial.println(" C");
+  /* Protect against unplugged or broken temp sensor */
+  if (current < 0) {
+    output = 0;
+  }
+
+  analogWrite(OUTPUT_PIN, output);
+
+  pcnt = (output * 100) / 255;
+  if (output == 0) {
+    sprintf(outchar, "Off");
+  } else {
+    sprintf(outchar,"%d%%", pcnt);
+  }
+
+  refresh_display(curchar, setchar, outchar);
+
+  /* Limit logging rate */
+  if (now - log_time > 1000) {
+    log_time = log_time + 1000;
+    Serial.print(setpoint);
+    Serial.print(", ");
+    Serial.print(output);
+    Serial.print(", ");
+    Serial.println(current);
+  }
 }
---
+++ README.md
@@ -0,0 +1,48 @@
+# Brew Temperature Controller
+
+This project is a small heater controller that I have made to enable a few spare 3D printer parts I had lying arround to be used to control the temperature of my beer when brewing. I had been using a full RAMPS1.4 controller with a 4 line display, but this was very bulky. Rather than the Arduino Mega utilised with the RAMPS board, this uses an Arduino Uno, which is much smaller.
+
+The device is fully powered from the PSU that's needed to supply power to the heater (this can not be pulled from the Arduino). If plugged in via USB, the set temperature, power output (scaled from 0-255) and current temperature are logged approximately once a second.
+
+## PCB Ideas
+
+- Improve layout of MOSFET now a more powerful transistor is being used.
+- Getting noise back in (cheap) power supply, better smoothing?
+
+## Code Ideas
+
+- Better PID library
+
+## Parts
+
+- Parts below from CPC (cpc.farnell.com):
+
+```
+Order Code	Qty	Manufacturer Part Number	Manufacturer / Description									Line Price Ex. VAT (Inc. Vat)
+SW02467		2	MCDTS6-1R			MULTICOMP PRO  Switch, Tactile 4.3mm 260g							0.22
+CN21011		1	CTBP9358/4			CAMDENBOSS  5.08mm Horizontal Pluggable Terminal Block Header, 4-Pole, 15A			0.43
+CN19453		1	CTB0708/2			CAMDENBOSS  PCB Terminal, High Power, 5.08mm, 2 Way						0.49
+FF02092		2	MCCQ-122			MULTICOMP PRO  Fuseholder, PCB Mount, Mini Blade						0.48
+CA08185		1	MCMR50V106M5X7			MULTICOMP PRO  Electrolytic Capacitor, 10 µF, 50 V, MCMR Series, ± 20%, Radial Leaded, 5 mm	0.05
+SC11131		1	STP60NF06L			STMICROELECTRONICS  Power MOSFET, N Channel, 60 V, 60 A, 0.014 ohm, TO-220, Through Hole	1.36
+```
+
+- Nokia 5110 style screen with the following pinout:
+
+```
+Pin	Use
+1	RST
+2	CE
+3	DC
+4	DIN
+5	CLK
+6	VCC
+7	LIGHT
+8	GND
+```
+
+- Arduino Uno, cheap clone used
+- Fuses: Standard Car mini Fuses. Used 5A (smallest I had) for electronics, 20A for heater output.
+- Heater I found a standard 22cm circular 3D printer heated bed fits nicely under my brew bucket (it was this that inspired the build).
+- Thermistor: Standard 100K thermistor, as used on many 3D printers.
+- PSU: Currently using spare 0.8A 12v PSU as might be used on a small 3D printer.
